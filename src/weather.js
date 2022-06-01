05fe9f67c455c3d1431c807bc3a18b32d09a1bc4
Pader22 <102009133+Pader22@users.noreply.github.com>
2022-06-01 18:57:29 -0500

    display daily forecast by using API

--- src/weather.js
+++ src/weather.js
@@ -23,31 +23,44 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let dailyForecast = response.data.daily;
   let forecast = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  dailyForecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
   
     <div class="col-2">
-      <div class="forecast-days" id="forecast-days">
-        ${day}
+      <div class="forecast-days" >
+        ${formatDay(forecastDay.dt)}
       </div>
-
+    
       <img
-        src="http://openweathermap.org/img/wn/04n@2x.png"
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt=""
         width="40"
       />
-      <div class="max-temp">60°</div>
+      <div class="max-temp" >${Math.round(forecastDay.temp.max)}°</div>
 
-      <div class="min-temp">19°</div>
+      <div class="min-temp" >${Math.round(forecastDay.temp.min)}°</div>
+      
     </div>`;
+    }
+    console.log(Math.round(forecastDay.temp.min));
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -63,7 +76,6 @@ function getDailyForecast(coordinates) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperature = document.querySelector("#temp-change");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#windSpeed");
@@ -127,6 +139,7 @@ let form = document.querySelector("#search-city");
 form.addEventListener("submit", searchCity);
 
 function currentCityTemp(response) {
+  console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   let humid = response.data.main.humidity;
   let humidity = document.querySelector("#humidity");
@@ -144,6 +157,8 @@ function currentCityTemp(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getDailyForecast(response.data.coord);
 }
 
 function cityName(position) {
